17028bcd4551f7fedd8ca63e33c181e1a310e991
Florian TRECUL <trecul.florian@gmail.com>
2021-03-03 09:45:42 +0100

    Add images and the puppy model with the data of the fake puppies

---
+++ app/src/main/java/com/example/androiddevchallenge/data/PuppyFactory.kt
@@ -0,0 +1,110 @@
+package com.example.androiddevchallenge.data
+
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.model.Puppy
+
+object PuppyFactory {
+
+    val puppyList = listOf(
+        Puppy(
+            id = 0,
+            name = "Thor",
+            age = 3.5,
+            gender = "Male",
+            race = "Berger",
+            color = "Brown",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_1
+        ),
+        Puppy(
+            id = 1,
+            name = "Luna",
+            age = 3.5,
+            gender = "Female",
+            race = "Golden Retriever",
+            color = "Black",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_2
+        ),
+        Puppy(
+            id = 2,
+            name = "Jack",
+            age = 2.5,
+            gender = "Male",
+            race = "Shiba Inu",
+            color = "Orange",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_3
+        ),
+        Puppy(
+            id = 3,
+            name = "Chanel",
+            age = 6.0,
+            gender = "Female",
+            race = "Labrador",
+            color = "Grey",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_4
+        ),
+        Puppy(
+            id = 4,
+            name = "Diego",
+            age = 4.0,
+            gender = "Male",
+            race = "Berger",
+            color = "Grey",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_5
+        ),
+        Puppy(
+            id = 5,
+            name = "Rio",
+            age = 8.0,
+            gender = "Female",
+            race = "Berger",
+            color = "Orange",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_6
+        ),
+        Puppy(
+            id = 6,
+            name = "Leo",
+            age = 5.0,
+            gender = "Male",
+            race = "Berger",
+            color = "Grey",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_7
+        ),
+        Puppy(
+            id = 7,
+            name = "Leo",
+            age = 5.0,
+            gender = "Male",
+            race = "Shiba Inu",
+            color = "Orange",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_8
+        ),
+        Puppy(
+            id = 8,
+            name = "Rox",
+            age = 5.0,
+            gender = "Female",
+            race = "Border Collie",
+            color = "Black/White",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_9
+        ),
+        Puppy(
+            id = 10,
+            name = "Rhino",
+            age = 9.0,
+            gender = "Male",
+            race = "Sharpey",
+            color = "Brown",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+            image = R.drawable.puppy_10
+        )
+    )
+}
---
+++ app/src/main/java/com/example/androiddevchallenge/model/Puppy.kt
@@ -0,0 +1,12 @@
+package com.example.androiddevchallenge.model
+
+data class Puppy(
+    val id: Int,
+    val name: String,
+    val age: Double,
+    val gender: String,
+    val race: String,
+    val color: String,
+    val image: Int,
+    val description: String,
+)
